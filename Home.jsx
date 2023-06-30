49787887401f114710ca07202c994c488583055a
Ruthran007 <131639055+Ruthran007@users.noreply.github.com>
2023-06-30 08:19:23 +0530

    Add files via upload

---
+++ Home.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import home from "../images/home.svg";
+import Common from './Common';
+
+const Home = () => {
+    return (
+        <>
+            <Common 
+                name='Grow your business with' 
+                imgsrc={home} 
+                isCompName={true}
+                compName="Company Name"
+                visit='/services' 
+                btnname="Get Started" 
+            />
+        </>
+    )
+}
+
+export default Home;import React from 'react';
+import home from "../images/home.svg";
+import Common from './Common';
+
+const Home = () => {
+    return (
+        <>
+            <Common 
+                name='Grow your business with' 
+                imgsrc={home} 
+                isCompName={true}
+                compName="Company Name"
+                visit='/services' 
+                btnname="Get Started" 
+            />
+        </>
+    )
+}
+
+export default Home;
